c30a994ff188c28161eb585c23f3847441181525
Timothy Moore <mtimothy984@gmail.com>
2023-01-13 09:01:51 -0800

    Gentler shutdown

--- scripts/auto/stop.sh
+++ scripts/auto/stop.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-screen -S webapp -X quit
+screen -S webapp -X stuff "^C"
 if ! nginx -s quit
 then
     echo "Failed to quit nginx, attempting to kill by process name"
@@ -19,19 +19,23 @@ done
 
 echo "nginx stopped successfully"
 
-if [ -f updater.lock ]
-then
-    echo "Updater.lock still exists, giving it another 5 seconds"
-    local ctr=0
-    while [ -f updater.lock ]
-    do
-        sleep 1
-        ctr=$((ctr+1))
-        if [ $ctr -gt 5 ]
-        then
-            echo "Updater is taking too long to finish, yoinking"
-            rm -f updater.lock
-            break
-        fi
-    done
-fi
+stop_updater() {
+  if [ -f updater.lock ]
+  then
+      echo "Updater.lock still exists, giving it another 5 seconds"
+      local ctr=0
+      while [ -f updater.lock ]
+      do
+          sleep 1
+          ctr=$((ctr+1))
+          if [ $ctr -gt 5 ]
+          then
+              echo "Updater is taking too long to finish, yoinking"
+              rm -f updater.lock
+              break
+          fi
+      done
+  fi
+}
+
+stop_updater()
