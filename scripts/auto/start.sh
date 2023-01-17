e2d504c647481fd25027a2b8ec90d04a34d0ffa5
Timothy Moore <mtimothy984@gmail.com>
2023-01-17 08:57:32 -0800

    Wait for updater.lock before starting nginx

--- scripts/auto/start.sh
+++ scripts/auto/start.sh
@@ -1,3 +1,21 @@
 #!/usr/bin/env bash
-nginx
 screen -dmS webapp ./scripts/run.sh
+
+wait_for_updater_lock() {
+  local ctr=0
+  while [ ! -f updater.lock ]
+  do
+    echo "Waiting for updater.lock"
+
+    ctr=$((ctr+1))
+    if [ $ctr -gt 30 ]
+    then
+      echo "Timeout waiting for updater.lock"
+      return 1
+    fi
+    sleep 1
+  done
+}
+
+wait_for_updater_lock
+nginx
