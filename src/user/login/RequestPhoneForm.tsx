af83e6ad9d7b566b9b56daad1e8fbe839222698a
Timothy Moore <mtimothy984@gmail.com>
2023-03-14 05:33:56 -0700

    Update request phone text

--- src/user/login/RequestPhoneForm.tsx
+++ src/user/login/RequestPhoneForm.tsx
@@ -267,10 +267,10 @@ export const RequestPhoneForm = ({
               </div>
 
               <div className={styles.getNotifiedContainer}>
-                By continuing you agree to receive reminders and marketing offers from Oseh. Msg &
-                data rates may apply. Approx. 1 message per day. Text HELP for help or STOP to
-                cancel. <a href="https://www.oseh.com/privacy">Privacy Policy</a> &{' '}
-                <a href="https://www.oseh.com/terms">Terms of Service</a>.
+                By continuing you agree to our <a href="https://www.oseh.com/terms">Terms</a> and{' '}
+                <a href="https://www.oseh.com/privacy">Privacy Policy</a>, and to receive marketing
+                messages from Oseh. Msg & data rates may apply. Approx. 1 message per day. Text HELP
+                for help or STOP to cancel.
               </div>
 
               {error && (
