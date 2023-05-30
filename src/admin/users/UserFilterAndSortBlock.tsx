8aca4e31bcde6d9efbd92700623074eaeee43cf3
Timothy Moore <mtimothy984@gmail.com>
2023-05-30 12:49:49 -0700

    Filter users by attributable utms

--- src/admin/users/UserFilterAndSortBlock.tsx
+++ src/admin/users/UserFilterAndSortBlock.tsx
@@ -143,6 +143,96 @@ export const UserFilterAndSortBlock = ({
     [setFilter, createdAtStartDateInLocalTime, createdAtEndDateInLocalTime]
   );
 
+  const [filteringUTM, utmSource, utmMedium, utmCampaign, utmTerm, utmContent] = useMemo<
+    [boolean, string, string, string, string, string]
+  >(() => {
+    if (filter.utm === null || filter.utm === undefined) {
+      return [false, '', '', '', '', ''];
+    }
+
+    if (filter.utm.operator !== 'ilike') {
+      return [false, '', '', '', '', ''];
+    }
+
+    const filterRepresentation = filter.utm.value;
+    const canonicalRepresentation = ((f) => {
+      if (f.startsWith('%')) {
+        f = f.slice(1);
+      }
+      // replace all % which aren't escaped with \\ with &. this will not work
+      // with \\\\% but that's so unlikely to happen that it's not worth
+      // worrying about
+      f = f.replace(/(?<!\\)%/g, '&');
+      return f;
+    })(filterRepresentation);
+    const parsed = new URLSearchParams(canonicalRepresentation);
+
+    return [
+      true,
+      parsed.get('utm_source') || '',
+      parsed.get('utm_medium') || '',
+      parsed.get('utm_campaign') || '',
+      parsed.get('utm_term') || '',
+      parsed.get('utm_content') || '',
+    ];
+  }, [filter.utm]);
+
+  const setUTMFilter = useCallback(
+    (
+      checked: boolean,
+      newUtmSource?: string,
+      newUtmMedium?: string,
+      newUtmCampaign?: string,
+      newUtmTerm?: string,
+      newUtmContent?: string
+    ) => {
+      setFilter((oldFilter) => {
+        if (checked) {
+          const source = newUtmSource ?? utmSource;
+          const medium = newUtmMedium ?? utmMedium;
+          const campaign = newUtmCampaign ?? utmCampaign;
+          const term = newUtmTerm ?? utmTerm;
+          const content = newUtmContent ?? utmContent;
+
+          let searchTerm = '';
+          if (campaign !== '') {
+            searchTerm += `utm_campaign=${encodeURIComponent(campaign).replaceAll('%', '\\%')}%`;
+          } else {
+            searchTerm += '%';
+          }
+
+          if (content !== '') {
+            searchTerm += `utm_content=${encodeURIComponent(content).replaceAll('%', '\\%')}%`;
+          }
+          if (medium !== '') {
+            searchTerm += `utm_medium=${encodeURIComponent(medium).replaceAll('%', '\\%')}%`;
+          }
+          if (source !== '') {
+            searchTerm += `utm_source=${encodeURIComponent(source).replaceAll('%', '\\%')}%`;
+          }
+          if (term !== '') {
+            searchTerm += `utm_term=${encodeURIComponent(term).replaceAll('%', '\\%')}%`;
+          }
+
+          return {
+            ...oldFilter,
+            utm: {
+              operator: 'ilike',
+              value: searchTerm,
+            },
+          };
+        } else {
+          const res = { ...oldFilter };
+          if (res.utm !== undefined) {
+            delete res.utm;
+          }
+          return res;
+        }
+      });
+    },
+    [setFilter, utmSource, utmMedium, utmCampaign, utmTerm, utmContent]
+  );
+
   const onCreatedAtStartDateChange = useCallback(
     (e: React.ChangeEvent<HTMLInputElement>) => {
       if (e.target.valueAsDate) {
@@ -313,6 +403,68 @@ export const UserFilterAndSortBlock = ({
                 onChange={onCreatedAtEndDateChange}
               />
             </CrudFormElement>
+          </div>
+        </CrudFormElement>
+        <CrudFormElement title="UTM">
+          <Checkbox value={filteringUTM} setValue={setUTMFilter} label="Filter" />
+          <div
+            className={styles.utmContainer}
+            style={filteringUTM ? undefined : { display: 'none' }}>
+            <TextInput
+              label="Source"
+              value={utmSource}
+              onChange={(newVal) => {
+                setUTMFilter(true, newVal);
+              }}
+              help={null}
+              disabled={false}
+              inputStyle="normal"
+              html5Validation={null}
+            />
+            <TextInput
+              label="Medium"
+              value={utmMedium}
+              onChange={(newVal) => {
+                setUTMFilter(true, undefined, newVal);
+              }}
+              help={null}
+              disabled={false}
+              inputStyle="normal"
+              html5Validation={null}
+            />
+            <TextInput
+              label="Campaign"
+              value={utmCampaign}
+              onChange={(newVal) => {
+                setUTMFilter(true, undefined, undefined, newVal);
+              }}
+              help={null}
+              disabled={false}
+              inputStyle="normal"
+              html5Validation={null}
+            />
+            <TextInput
+              label="Term"
+              value={utmTerm}
+              onChange={(newVal) => {
+                setUTMFilter(true, undefined, undefined, undefined, newVal);
+              }}
+              help={null}
+              disabled={false}
+              inputStyle="normal"
+              html5Validation={null}
+            />
+            <TextInput
+              label="Content"
+              value={utmContent}
+              onChange={(newVal) => {
+                setUTMFilter(true, undefined, undefined, undefined, undefined, newVal);
+              }}
+              help={null}
+              disabled={false}
+              inputStyle="normal"
+              html5Validation={null}
+            />
           </div>
         </CrudFormElement>
       </div>
