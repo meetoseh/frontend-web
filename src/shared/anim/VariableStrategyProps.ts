6bca5c01c5a6fedad0d46a0d9c4a5b0f191cec9c
Timothy Moore <mtimothy984@gmail.com>
2023-07-03 08:16:28 -0700

    Add missing hook documentation

--- src/shared/anim/VariableStrategyProps.ts
+++ src/shared/anim/VariableStrategyProps.ts
@@ -13,6 +13,55 @@ export type VariableStrategyProps<P> =
   | { type: 'react-rerender'; props: P }
   | { type: 'callbacks'; props: () => P; callbacks: Callbacks<undefined> };
 
+/**
+ * Converts a VariablyStrategyProps, which is usually used as the type that's
+ * passed in as a prop to a component, into a ValueWithCallbacks, which is
+ * a simplified version of the 'callbacks' option, which changes without
+ * triggering a react rerender.
+ *
+ * Example:
+ *
+ * ```tsx
+ * const MyComponent = (
+ *   { propsVariableStrategy }: {
+ *     propsVariableStrategy: VariableStrategyProps<{ textContent: string }>;
+ *   }
+ * ): ReactElement => {
+ *   const props = useVariableStrategyPropsAsValueWithCallbacks(propsVariableStrategy);
+ *   const containerRef = useRef<HTMLDivElement>(null);
+ *
+ *   useEffect(() => {
+ *     props.callbacks.add(render);
+ *     render();
+ *     return () => {
+ *       if (containerRef.current === null) {
+ *         return;
+ *       }
+ *       containerRef.current.textContent = props.get().textContent;
+ *     }
+ *   })
+ *
+ *   return <div ref={containerRef} />;
+ * };
+ *
+ * // usage example, using react rerender strategy
+ * <MyComponent propsVariableStrategy={{
+ *   type: 'react-rerender',
+ *   props: { textContent: 'hello' }
+ * }} />
+ *
+ * // alternatively, using callbacks strategy
+ * const componentProps = useWritableValueWithCallbacks({ textContent: 'hello' });
+ * <MyComponent propsVariableStrategy={{
+ *   type: 'callbacks',
+ *   props: componentProps.get,
+ *   callbacks: componentProps.callbacks
+ * }} />
+ * ```
+ *
+ * @param props The variable strategy props to adapt
+ * @returns The adapted props using the least common denominator, ValueWithCallbacks
+ */
 export const useVariableStrategyPropsAsValueWithCallbacks = <P>(
   props: VariableStrategyProps<P>
 ): ValueWithCallbacks<P> => {
