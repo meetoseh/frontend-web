523b1bf17152d6aa15c459bf637bc9101a99f178
Timothy Moore <mtimothy984@gmail.com>
2024-07-25 13:42:39 -0700

    Request screen version 70

--- src/shared/lib/screenVersion.ts
+++ src/shared/lib/screenVersion.ts
@@ -3,4 +3,4 @@
  * to be equivalent to. This is provided to all peek / pop related requests,
  * and the backend handles mapping it to the correct screens.
  */
-export const SCREEN_VERSION = 67;
+export const SCREEN_VERSION = 70;
