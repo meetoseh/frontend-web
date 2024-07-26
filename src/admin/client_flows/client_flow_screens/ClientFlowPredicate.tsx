b4b11809239b36bb0d75133fe26032ecc9da67b6
Timothy Moore <mtimothy984@gmail.com>
2024-07-26 11:25:54 -0700

    add sticky random groups / random float

--- src/admin/client_flows/client_flow_screens/ClientFlowPredicate.tsx
+++ src/admin/client_flows/client_flow_screens/ClientFlowPredicate.tsx
@@ -6,14 +6,24 @@ import { CrudFetcherMapper, SimpleFilterItem } from '../../crud/CrudFetcher';
  * as a whole to be true.
  */
 export type ClientFlowPredicate = {
-  /** If provided and not null, the restriction on the client-provided version hint */
+  /** client-provided version hint, generally matches an android version code */
   version?: SimpleFilterItem | null;
+  /** time spent in the queue in seconds */
   timeInQueue?: SimpleFilterItem | null;
+  /** time since the users account was created in seconds */
   accountAge?: SimpleFilterItem | null;
+  /**
+   * each group gives a random 0 or 1 value for every user. group names are case insensitive
+   * and created if missing. group names are not normalized into titleCase, so prefer values
+   * that are the same in titleCase and snake_case. where not possible, use snake_case.
+   */
+  stickyRandomGroups?: { [groupName: string]: SimpleFilterItem } | null;
+  /** a random float between 0 and 1 */
+  randomFloat?: SimpleFilterItem | null;
 };
 
 export const clientFlowPredicateMapper: CrudFetcherMapper<ClientFlowPredicate> = (raw) => {
-  const result = {} as any;
+  const result: ClientFlowPredicate = {};
   if (raw.version !== undefined && raw.version !== null) {
     result.version = raw.version;
   }
@@ -23,7 +33,13 @@ export const clientFlowPredicateMapper: CrudFetcherMapper<ClientFlowPredicate> =
   if (raw.account_age !== undefined && raw.account_age !== null) {
     result.accountAge = raw.account_age;
   }
-  return result as ClientFlowPredicate;
+  if (raw.sticky_random_groups !== undefined && raw.sticky_random_groups !== null) {
+    result.stickyRandomGroups = raw.sticky_random_groups;
+  }
+  if (raw.random_float !== undefined && raw.random_float !== null) {
+    result.randomFloat = raw.random_float;
+  }
+  return result;
 };
 export const serializeClientFlowPredicate = (x: ClientFlowPredicate): any => ({
   ...(x.version !== undefined && x.version !== null ? { version: x.version } : {}),
@@ -31,4 +47,8 @@ export const serializeClientFlowPredicate = (x: ClientFlowPredicate): any => ({
     ? { time_in_queue: x.timeInQueue }
     : {}),
   ...(x.accountAge !== undefined && x.accountAge !== null ? { account_age: x.accountAge } : {}),
+  ...(x.stickyRandomGroups !== undefined && x.stickyRandomGroups !== null
+    ? { sticky_random_groups: x.stickyRandomGroups }
+    : {}),
+  ...(x.randomFloat !== undefined && x.randomFloat !== null ? { random_float: x.randomFloat } : {}),
 });
