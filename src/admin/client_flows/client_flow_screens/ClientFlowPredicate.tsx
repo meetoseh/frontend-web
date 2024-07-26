ece928dbf5a65b656fa6ca220851f7ef58003351
Timothy Moore <mtimothy984@gmail.com>
2024-07-26 08:08:31 -0700

    Add new client flow predicate variables

--- src/admin/client_flows/client_flow_screens/ClientFlowPredicate.tsx
+++ src/admin/client_flows/client_flow_screens/ClientFlowPredicate.tsx
@@ -8,9 +8,18 @@ import { CrudFetcherMapper, SimpleFilterItem } from '../../crud/CrudFetcher';
 export type ClientFlowPredicate = {
   /** If provided and not null, the restriction on the client-provided version hint */
   version?: SimpleFilterItem | null;
+  timeInQueue?: SimpleFilterItem | null;
+  accountAge?: SimpleFilterItem | null;
 };
 
-export const clientFlowPredicateMapper: CrudFetcherMapper<ClientFlowPredicate> = {};
+export const clientFlowPredicateMapper: CrudFetcherMapper<ClientFlowPredicate> = {
+  time_in_queue: 'timeInQueue',
+  account_age: 'accountAge',
+};
 export const serializeClientFlowPredicate = (x: ClientFlowPredicate): any => ({
   ...(x.version !== undefined && x.version !== null ? { version: x.version } : {}),
+  ...(x.timeInQueue !== undefined && x.timeInQueue !== null
+    ? { time_in_queue: x.timeInQueue }
+    : {}),
+  ...(x.accountAge !== undefined && x.accountAge !== null ? { account_age: x.accountAge } : {}),
 });
